c87d8d0478ebb272cfe06b2e69d58cd30c640439
Nicola Luca Karrer <N.Karrer.inf22@stud.bbbaden.ch>
2024-03-27 07:43:13 +0100

    Projektdateien hinzufügen.

---
+++ Rogue-Game/Core/Stairs.cs
@@ -0,0 +1,44 @@
+﻿using RLNET;
+using RogueSharp;
+using RogueSharpV3Tutorial.Core;
+using RogueSharpV3Tutorial.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rogue_Game.Core
+{
+    public class Stairs : IDrawable
+    {
+        public RLColor Color { get; set; }
+
+        public char Symbol { get; set; }
+
+        public int X { get; set; }
+
+        public int Y { get; set; }
+
+        public void Draw(RLConsole console, IMap map)
+        {
+            if (!map.GetCell(X, Y).IsExplored)
+            {
+                return;
+            }
+
+            Symbol = '<';
+            if (map.IsInFov(X, Y))
+            {
+                Color = Colors.Player;
+            }
+            else
+            {
+                Color = Colors.Floor;
+            }
+
+            console.Set(X, Y, Color, null, Symbol);
+        }
+
+    }
+}
